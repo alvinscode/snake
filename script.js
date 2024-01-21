3d238b6253abe444891668bd8206e82543784573
Alvin Li <aflip24@gmail.com>
2024-01-21 03:49:23 -0600

    add snake movement on key pressed

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const ctx = gameCanvas.getContext("2d");
 
 main();
 
+document.addEventListener("keydown", changeDirection)
+
 function clearCanvas(){
     ctx.fillStyle = GAME_BACKGROUND_COLOR;
     ctx.strokeStyle = GAME_BORDER_COLOR;
@@ -52,3 +54,36 @@ function main() {
     }, 100)
 }
 
+function changeDirection(event) {
+    const LEFT = 37;
+    const RIGHT = 39;
+    const UP = 38;
+    const DOWN = 40;
+
+    const keyPress = event.keyCode;
+    const goUp = dy === -10;
+    const goDown = dy === 10;
+    const goRight = dx ===10;
+    const goLeft = dx === -10;
+
+    if (keyPress === LEFT && !goRight) {
+        dx = -10;
+        dy = 0;
+    }
+
+    if (keyPress === UP && !goDown) {
+        dx = 0;
+        dy = -10;
+    }
+
+    if (keyPress === RIGHT && !goLeft) {
+        dx = 10;
+        dy = 0;
+    }
+
+    if (keyPress === DOWN && !goUp) {
+        dx = 0;
+        dy = 10;
+    }
+
+}   
